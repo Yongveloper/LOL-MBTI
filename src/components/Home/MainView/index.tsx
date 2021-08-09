2cb4876453de917e7d55cf4234bd9066f4b040dd
yongveloper <aaa9015@naver.com>
2021-08-09 11:16:08 +0900

    [기능] 시작하기 버튼 기능 추가

--- src/components/Home/MainView/index.tsx
+++ src/components/Home/MainView/index.tsx
@@ -3,11 +3,20 @@ import CopyBtn from 'src/components/common/Buttons/CopyBtn';
 import KakaoBtn from 'src/components/common/Buttons/KakaoBtn';
 import Title from './Title';
 
-const MainView = () => {
+interface IProps {
+  startHandler: () => void;
+}
+
+const MainView = ({ startHandler }: IProps) => {
   return (
     <main>
       <Title />
-      <Button bgColor="lightBlue" fontColor="white" borderColor="lightBlue">
+      <Button
+        bgColor="lightBlue"
+        fontColor="white"
+        borderColor="lightBlue"
+        onClick={startHandler}
+      >
         시작 하기!
       </Button>
       <KakaoBtn />
