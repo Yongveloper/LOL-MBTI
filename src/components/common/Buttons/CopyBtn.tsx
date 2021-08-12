92930f0ca922838d714637ee6647252068a7117a
Yongveloper <aaa9015@naver.com>
2021-08-12 23:07:41 +0900

    [기능] 링크 복사하기 기능 추가

--- src/components/common/Buttons/CopyBtn.tsx
+++ src/components/common/Buttons/CopyBtn.tsx
@@ -1,7 +1,39 @@
+import { useRef } from 'react';
+import styled from 'styled-components';
 import Button from '../Button';
 
+const Clipboard = styled.input.attrs({ type: 'text' })`
+  position: absolute;
+  top: 0;
+  left: 0;
+  opacity: 0;
+`;
+
 const CopyBtn = () => {
-  return <Button>링크 복사하기</Button>;
+  const copyUrlRef = useRef<HTMLInputElement>(null);
+  let currentUrl = '';
+
+  if (typeof window !== 'undefined') {
+    currentUrl = window.location.href;
+  }
+  console.log(currentUrl);
+  const copyHandler = () => {
+    if (!document.queryCommandSupported('copy')) {
+      return alert('복사하기가 지원되지 않는 브라우저입니다.');
+    }
+    if (copyUrlRef && copyUrlRef.current) {
+      copyUrlRef.current.select();
+      document.execCommand('copy');
+      alert('현재 주소가 클립보드에 복사되었습니다.');
+    }
+  };
+
+  return (
+    <>
+      <Button onClick={copyHandler}>링크 복사하기</Button>
+      <Clipboard ref={copyUrlRef} value={currentUrl} readOnly />
+    </>
+  );
 };
 
 export default CopyBtn;
