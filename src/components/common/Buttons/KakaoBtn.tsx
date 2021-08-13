c7940105fdf72a5ec60ca0f6c9f1dd0e662caa78
Yongveloper <aaa9015@naver.com>
2021-08-13 18:26:56 +0900

    [기능] 카카오 공유하기 기능 추가

--- src/components/common/Buttons/KakaoBtn.tsx
+++ src/components/common/Buttons/KakaoBtn.tsx
@@ -1,9 +1,17 @@
 import Button from '../Button';
 
 const KakaoBtn = () => {
-  console.log(process.env.NEXT_PUBLIC_KAKAO_API_KEY);
+  const onClick = () => {
+    if (typeof window == 'undefined') {
+      return '카카오로 공유하기에 실패했습니다.';
+    }
+    const { Kakao, location } = window;
+    Kakao.Link.sendScrap({
+      requestUrl: location.href,
+    });
+  };
   return (
-    <Button fontColor="yellow" borderColor="whiteYellow">
+    <Button fontColor="yellow" borderColor="whiteYellow" onClick={onClick}>
       카카오톡 공유하기
     </Button>
   );
