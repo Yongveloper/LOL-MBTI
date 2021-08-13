bb62679a5569758b4ca892eb9c18bdd6af43657a
Yongveloper <aaa9015@naver.com>
2021-08-14 00:14:12 +0900

    [기능] site_name meta tag 추가

--- pages/mbti/[type].tsx
+++ pages/mbti/[type].tsx
@@ -88,6 +88,7 @@ const Type = ({ type }: IProps) => {
           property="og:description"
           content="롤에서 알아보는 MBTI 유형과 해당 유형에 맞는 포지션을 찾아보세요!"
         />
+        <meta property="og:site_name" content="롤에서 알아보는 MBTI 유형" />
       </Head>
       <main>
         <SContent>
