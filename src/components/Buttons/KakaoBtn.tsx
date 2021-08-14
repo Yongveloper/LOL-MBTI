eb1a1c8eb0dcba6e0919d3117e89a0b2cc5178e4
Yongveloper <aaa9015@naver.com>
2021-08-14 13:31:33 +0900

    [스타일] 카카오톡 버튼 스타일 수정 가운데,세로 정렬, 간격

--- src/components/Buttons/KakaoBtn.tsx
+++ src/components/Buttons/KakaoBtn.tsx
@@ -1,5 +1,16 @@
 import Button from '../common/Button';
+import styled from 'styled-components';
 import KakaoIcon from 'public/images/kakaotalk_logo_icon_147272.svg';
+
+const SButton = styled(Button)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  svg {
+    margin-right: 6px;
+  }
+`;
 
 const KakaoBtn = () => {
   const onClick = () => {
@@ -12,15 +23,10 @@ const KakaoBtn = () => {
     });
   };
   return (
-    <Button fontColor="yellow" borderColor="whiteYellow" onClick={onClick}>
-      <KakaoIcon
-        viewBox="0 0 60 55"
-        width="30px"
-        height="30px"
-        style={{ marginRight: '8px' }}
-      />
+    <SButton fontColor="yellow" borderColor="whiteYellow" onClick={onClick}>
+      <KakaoIcon viewBox="0 0 60 48" width="30px" height="30px" />
       카카오톡 공유하기
-    </Button>
+    </SButton>
   );
 };
 
