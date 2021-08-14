83836e6d06735f35bcf66049db27594cc5869cb0
Yongveloper <aaa9015@naver.com>
2021-08-14 22:22:14 +0900

    [개선] SEO 최적화

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -41,14 +41,23 @@ export default class MyDocument extends Document {
     return (
       <Html lang="ko">
         <Head>
-          <script
-            defer
-            src="https://developers.kakao.com/sdk/js/kakao.min.js"
-          ></script>
           <meta
             name="google-site-verification"
             content="CYaoNc6HqLsOSdLZsPQPSOUW2UTLv8D94DLu6H485HI"
           />
+          <meta name="robots" content="ALL" />
+          <meta name="robots" content="index,follow" />
+          <meta name="author" content="yongyong" />
+          <meta name="reply-to" content="yongyong5766@gmail.com" />
+          <meta name="content-language" content="kr" />
+          <meta
+            name="keywords"
+            content="롤 MBTI, 롤에서 알아보는 MBTI 유형, MBTI, 롤 포지션 찾기, 롤 MBTI, lol mbti, lol mbti 유형"
+          />
+          <script
+            defer
+            src="https://developers.kakao.com/sdk/js/kakao.min.js"
+          ></script>
           <script
             async
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5623149944196012"
