f12365e4cdeab6d23785d7c1061fc7e24a48c429
Yongveloper <aaa9015@naver.com>
2021-08-17 20:07:55 +0900

    [구성] 홈 하단  카카오 애드핏 광고 추가

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import styled, { css } from 'styled-components';
 
 const Wrapper = styled.footer`
@@ -5,19 +6,43 @@ const Wrapper = styled.footer`
     return css`
       width: 100%;
       display: flex;
+      flex-direction: column;
+      align-items: center;
       justify-content: center;
       & span {
         font-size: ${theme.font.size.micro};
         color: ${theme.color.darkGray};
+        margin-top: ${theme.margin.base};
       }
     `;
   }}
 `;
 
-const Footer = () => (
-  <Wrapper>
-    <span>Contact: yongyong5766@gmail.com</span>
-  </Wrapper>
-);
+const Footer = () => {
+  useEffect(() => {
+    const adfitContainer = document.querySelector('.adfit');
+    const ins = document.createElement('ins');
+    const script = document.createElement('script');
+
+    ins.className = 'kakao_ad_area';
+    ins.style.display = 'none';
+    script.async = true;
+    script.type = 'text/javascript';
+    script.src = '//t1.daumcdn.net/kas/static/ba.min.js';
+    ins.setAttribute('data-ad-width', '320');
+    ins.setAttribute('data-ad-height', '250');
+    ins.setAttribute('data-ad-unit', 'DAN-kNNgmAXByO4ajNL6');
+    if (adfitContainer) {
+      adfitContainer.appendChild(ins);
+      adfitContainer.appendChild(script);
+    }
+  }, []);
+  return (
+    <Wrapper>
+      <div className="adfit" />
+      <span>Contact: yongyong5766@gmail.com</span>
+    </Wrapper>
+  );
+};
 
 export default Footer;
