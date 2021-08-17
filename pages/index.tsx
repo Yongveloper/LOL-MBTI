4e071e06b810efa011c81e81d47a4df4b92bc53d
Yongveloper <aaa9015@naver.com>
2021-08-17 18:10:57 +0900

    [변경] 중복 meta tag 제거

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,10 +12,6 @@ export default function Home() {
       <Head>
         <title>롤에서 알아보는 MBTI 유형 - 나에게 맞는 포지션은?</title>
         <meta property="og:title" content="롤에서 알아보는 MBTI 유형" />
-        <meta
-          property="og:description"
-          content="롤에서 알아보는 MBTI 유형과 해당 유형에 맞는 포지션을 찾아보세요!"
-        />
         <meta property="og:site_name" content="롤에서 알아보는 MBTI 유형" />
       </Head>
       {start ? <QuestionView /> : <MainView startHandler={startHandler} />}
