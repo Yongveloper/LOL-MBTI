8bd379e21ddef4ffb83fb5811179038a865bd218
yongveloper <aaa9015@naver.com>
2021-08-31 09:09:58 +0900

    [해결] crossorigin 오타 문제해결

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -71,7 +71,7 @@ export default class MyDocument extends Document {
           <script
             async
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5623149944196012"
-            crossorigin="anonymous"
+            crossOrigin="anonymous"
           ></script>
           <script
             defer
